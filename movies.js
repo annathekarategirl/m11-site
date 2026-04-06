9ccc223a92791df8ef56d5431f69348b834974b9
Anna Moore <annathekarategirl@gmail.com>
2026-04-06 13:08:49 -0400

    f

---
+++ movies.js
@@ -0,0 +1,34 @@
+// Define the Movie class
+const div=document.getElementById("movie-info")
+class Movie{
+  #title
+  #cast
+  #description
+  #rating
+  constructor(title,cast,description,rating){
+    this.#title=title;
+    this.#cast=cast;
+    this.#description=description;
+    this.#rating=rating;
+  }
+  updaterating(newrating){
+    this.#rating=newrating
+  }
+  display(){
+    div.innerHTML="Title: "+this.#title+"<br>Cast: "+this.#cast+"<br>Description: "+this.#description+'<br>Rating: '+this.#rating
+  }
+}
+  // title, cast, description, rating
+
+  // Method to update the rating of the movie
+
+  // Method to display the movie's information
+
+
+// Test your implementation
+
+// Display the initial information of the movie
+
+// Update the rating of the movie
+
+// Display the updated information of the movie
